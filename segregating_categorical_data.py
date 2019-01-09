e3c492edb2424c07d65baafc16ec94f0d4025faa
Eric Schles <Ericschles@gmail.com>
2019-01-09 17:34:41 -0500

    Update segregating_categorical_data.py

--- segregating_categorical_data.py
+++ segregating_categorical_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import code
        
 def base_agreement_criteria(sample, df, columns):
     count = 0
     for column in columns:
-        if sample[column].mode() == df[column].mode():
+        if int(sample[column].mode()) == int(df[column].mode()):
             count +=1
     return count
 
@@ -27,7 +27,7 @@ def nuanced_agreement_criteria(sample, df, columns):
         column_diffs[column] = summed_diff_in_percent
     return column_diffs
     
-def generate_representative_sample(df, columns, sample_size=100, num_iterations=1000):
+def generate_representative_sample(df, columns, sample_size=1000, num_iterations=1000, simple=False):
     """
     This function generates a representative random sample based 
     on specific variables in the data set.
@@ -44,27 +44,30 @@ def generate_representative_sample(df, columns, sample_size=100, num_iterations=
     If the moment differencing method is used, we search for the sample which minimizes
     difference between the first two moments.
     Notice that we only select on moment differences if ks fails for all generated samples.
-    """
-    
+    """    
     possible_samples = []
     base_agreement_scores = []
     nuanced_agreement_scores = []
     for _ in range(num_iterations):
         sample = df.sample(sample_size)   
         possible_samples.append(sample)
+        base_agreement_scores.append(
+            base_agreement_criteria(sample, df, columns))
         nuanced_score = nuanced_agreement_criteria(sample, df, columns)
         nuanced_agreement_scores.append(
             sum(list(nuanced_score.values()))
-        )
-        #base_agreement_scores.append(
-        #    base_agreement_criteria(sample, df, columns))
-        
-    best_score = nuanced_agreement_scores[0]
+                    
+    best_nuanced_score = nuanced_agreement_scores[0]
+    best_base_score = base_agreement_scores[0]
     best_sample = possible_samples[0]
-    for index, value in enumerate(agreement_scores):
-        if value < best_score:
-            best_score = value
+    for index, value in enumerate(nuanced_agreement_scores):
+        if value < best_nuanced_score:
+            best_nuanced_score = value
             best_sample = possible_samples[index]
+    # for index, value in enumerate(base_agreement_scores):
+    #     if value > best_base_score:
+    #         best_base_score = value
+    #         best_sample = possible_samples[index]
     return best_sample
 
 def remove_by_index(df, sample):
@@ -72,13 +75,17 @@ def remove_by_index(df, sample):
     
 df = pd.read_csv("PLACE YOUR DATA HERE.csv")
 
-# fill this in with the columns of interest
+# columns of interest here
 columns = []
 
 # training, testing, validation, holdout (not listed)
 sample_sizes = [int(len(df)*0.60), int(len(df)*0.15), int(len(df)*0.15)]
 samples = []
 for index, sample_size in enumerate(sample_sizes):
-    samples.append(generate_representative_sample(df, columns, sample_size=sample_size))
+    if index == 0:
+        samples.append(generate_representative_sample(df, columns, sample_size=sample_size, num_iterations=1))
+    else:
+        samples.append(generate_representative_sample(df, columns, sample_size=sample_size, num_iterations=1))
     df = remove_by_index(df, samples[index])
-code.interact(local=locals())  
+
+
